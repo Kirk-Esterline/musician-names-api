d0557f4a13002c95582a62f5c1de2873feeb8132
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-02-27 14:08:20 -0500

    initial commit for musician-names-api

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    "21 savage": {
+        'age':29,
+        'birthname':'Sheyaa Bin Abraham-Joseph',
+        'birthlocation': 'London, England',
+    },
+    "chance the rapper": {
+        'age':29,
+        'birthname':'Chancelor Bennett',
+        'birthlocation': 'Chicago, Illinois',
+    },
+    "dylon": {
+        'age':29,
+        'birthname':'Dylon',
+        'birthlocation': 'Dylon, Dylon',
+    },
+    "unknown": {
+        'age': 0,
+        'birthname': 'unknown',
+        'birthlocation': 'unknown',
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req, res) => {
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    } else {
+        res.json(rappers['Unknown'])
+    }
+})
+
+app.listen(PORT, () =>{
+    console.log(`The server is running on port ${PORT}! You better go catch it...`)
+})
