a7b23ade57a391612c930cb6c54825a6bea571f6
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-02-27 14:24:18 -0500

    corrected spelling error causing unknown names to return blank rather than 'unknown'

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/api/:rapperName', (req, res) => {
     if(rappers[rappersName]){
         res.json(rappers[rappersName])
     } else {
-        res.json(rappers['Unknown'])
+        res.json(rappers['unknown'])
     }
 })
 
