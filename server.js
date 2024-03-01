0d6324247a8cf65752fb65df8502f76c23803b06
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-03-01 18:13:43 -0500

    completed connection to Mongodb. The remainder of the interface still needs to be completed.

--- server.js
+++ server.js
@@ -5,17 +5,37 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb+srv://kirkesterline:FxViYSOIP6BfHHIY@musicians2.qgkw14k.mongodb.net/?retryWrites=true&w=majority&appName=Musicians2')
-    .then(client => {
-        console.log('Connected to Database')
-    })
-    .catch(error => console.error(error))
-    
-//Mongodb password: FxViYSOIP6BfHHIY
-
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect('mongodb+srv://kirkesterline:FxViYSOIP6BfHHIY@musicians2.qgkw14k.mongodb.net/?retryWrites=true&w=majority&appName=Musicians2')
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('Musicians2')
+        const musiciansCollection = db.collection('musicians')
+        
+        app.post('/musicians', (req,res) => {
+            musiciansCollection
+                .insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.get('/', (req, res) => {
+            db.collection('musicians')
+                .find()
+                .toArray()
+                .then(results => {
+                    console.log(results)
+                })
+                .catch(error => console.error(error))
+        })
+    })
+    .catch(error => console.error(error))
+
 
 const rappers = {
     "21 savage": {
@@ -39,14 +59,6 @@ const rappers = {
         'birthlocation': 'unknown',
     }
 }
-
-app.post('/musicians', (req,res) => {
-    console.log(req.body)
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
 
 app.get('/api/:rapperName', (req, res) => {
     console.log(req.body)
